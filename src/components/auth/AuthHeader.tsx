19761fa4857f65bb599cec22d679597851757b8f
Derian Melvin <derianmelvin88@gmail.com>
2023-06-15 18:34:09 +0700

    Update AuthHeader

--- src/components/auth/AuthHeader.tsx
+++ src/components/auth/AuthHeader.tsx
@@ -1,9 +1,16 @@
+import { Outlet } from "react-router-dom";
 import { Logo } from "../Logo";
 
-export const Header = () => {
+export const AuthHeader = () => {
   return (
-    <header className="flex flex-col items-center px-5 py-5 pb-16 bg-[#19222E]">
-      <Logo />
-    </header>
+    <>
+      <header className="flex flex-col items-center px-5 py-5 pb-16 bg-[#19222E]">
+        <div className="flex items-center gap-4">
+          <Logo />
+          <h1 className="text-3xl text-[#1371FF] font-semibold">Minicrate</h1>
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
 };
