49526f4b7526198944e31af6c40286595de445f1
Derian Melvin <derianmelvin88@gmail.com>
2023-06-15 14:57:54 +0700

    Import components & setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { AuthHeader } from "./components/auth/AuthHeader";
+import { SignInForm } from "./components/auth/SignInForm";
+import { SignUpForm } from "./components/auth/SignUpForm";
+import { ErrorPage } from "./components/ErrorPage";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello</h1>
+    <div className="min-h-screen flex flex-col items-center bg-[#1A5BB7]">
+      <div className="w-full flex flex-col">
+        <Routes>
+          <Route path="/" element={<AuthHeader />}>
+            <Route path="login" element={<SignInForm />} />
+            <Route path="register" element={<SignUpForm />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
