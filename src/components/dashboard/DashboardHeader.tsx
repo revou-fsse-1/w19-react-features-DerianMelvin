13b662c3972e4cda18f6182d649cd7358907f33d
Derian Melvin <derianmelvin88@gmail.com>
2023-06-16 22:34:49 +0700

    Update DashboardHeader

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../Logo";
 
 export const DashboardHeader = () => {
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    navigate("../");
+  };
+
   return (
     <header className="min-w-fit flex items-center justify-between gap-4 px-5 py-5 pb-16 bg-[#19222E]">
-      <Logo />
-      <span className="text-center text-white font-medium">Hello, USERNAME</span>
-      <button className="px-5 py-2 rounded-lg text-lg font-semibold bg-gray-50">
+      <div onClick={() => navigate("../dashboard")} className="cursor-pointer">
+        <Logo />
+      </div>
+      <button
+        onClick={() => logoutUser()}
+        className="px-5 py-2 rounded-lg text-lg font-semibold bg-gray-50"
+      >
         Logout
       </button>
     </header>
