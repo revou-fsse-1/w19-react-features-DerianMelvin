4880a3b79c088b5a46ac82f2f6180a05d52bb921
Derian Melvin <derianmelvin88@gmail.com>
2023-06-16 17:06:51 +0700

    Adjust component routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import { AuthHeader } from "./components/auth/AuthHeader";
 import { SignInForm } from "./components/auth/SignInForm";
 import { SignUpForm } from "./components/auth/SignUpForm";
 import { ErrorPage } from "./components/ErrorPage";
+import { Dashboard } from "./components/dashboard/Dashboard";
+import { EditData } from "./components/dashboard/EditData";
+import { AuthRoute } from "./components/AuthRoute";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center bg-[#1A5BB7]">
       <div className="w-full flex flex-col">
         <Routes>
-          <Route path="/" element={<AuthHeader />}>
-            <Route path="login" element={<SignInForm />} />
-            <Route path="register" element={<SignUpForm />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
+          <Route path="/" element={<AuthRoute />} />
+          <Route path="/login" element={<SignInForm />} />
+          <Route path="/register" element={<SignUpForm />} />
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/:id" element={<EditData />} />
         </Routes>
       </div>
     </div>
